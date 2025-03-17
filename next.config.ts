ad5ed5bd9283c34591729457f1a2303d1f1c63ab
Khoa Nguyen <colormak3r@gmail.com>
2025-03-17 08:29:28 -0700

    Update gitignore and export settings

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'dist',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
